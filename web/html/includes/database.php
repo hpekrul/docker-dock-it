1b70598effef57d52172cdd151db72997ae24ca4
hpekrul <hpekrul@my.wctc.edu>
2024-01-31 20:14:02 -0600

    updated env variables

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,4 +1,8 @@
 <?php
 //      server        username          password              database
-$db = @mysqli_connect('localhost', 'hpekrul', '000471208', 'hpekrul')
+$db = @mysqli_connect(
+    getenv("MYSQL_HOST"),
+    getenv("MYSQL_USER"),
+    getenv("MYSQL_PASSWORD"),
+    getenv("MYSQL_DATABASE"))
 or die('unable to connect to the database.');
